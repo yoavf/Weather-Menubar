f2cd2bc1d59ebbf2ad18f7e756f505284f02cd6f
Yoav Farhi <yoav@farhi.org>
2023-11-12 20:38:40 +0200

    Initial Commit

---
+++ Weather-Tomorrow.io/Weather_Tomorrow_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Tomorrow_ioApp.swift
+//  Weather-Tomorrow.io
+//
+//  Created by Yoav Farhi on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Tomorrow_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
